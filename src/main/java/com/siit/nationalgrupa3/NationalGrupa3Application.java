083b630fe7767e08353241c08978eb77527ffe28
nicolae.spalatelu <nicolae.spalatelu@gohenry.co.uk>
2021-03-03 20:12:39 +0200

    initial commit

---
+++ src/main/java/com/siit/nationalgrupa3/NationalGrupa3Application.java
@@ -0,0 +1,13 @@
+package com.siit.nationalgrupa3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalGrupa3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NationalGrupa3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/siit/nationalgrupa3/NationalGrupa3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siit.nationalgrupa3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalGrupa3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
